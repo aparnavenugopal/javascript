be02258907ba01bba6a6c99bc4c2ca0901db1f04
uday surya <udaysurya1597@gmail.com>
2025-05-19 17:25:37 +0530

    learning dev js

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!😉'
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent =35;
+// document.querySelector('.guess').value = 23;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', () => {
+    const guess = Number(document.querySelector('.guess').value);
+    if(!guess){
+        document.querySelector('.message').textContent = '🚫  no number!'
+    }else if(guess === secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number!😉';
+    }else if(guess > secretNumber){
+        if(score > 1){
+           document.querySelector('.message').textContent =  '📈 Too High!';
+           score--;
+           document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = '😭 you lost the game!'
+        }
+       
+    }else if(guess < secretNumber){
+        if(score > 1){
+           document.querySelector('.message').textContent = '📉 Too low!';
+           score--;
+           document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = '😭 you lost the game!'
+        }
+       
+    }
+})
